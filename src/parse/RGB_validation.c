313c5805f6f783f6c05ea247f61f93c50fe4f005
Simon Langerock <slangero@student.s19.be>
2025-05-08 11:41:25 +0200

    add file for RGB validation

---
+++ src/parse/RGB_validation.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RGB_validation.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slangero <slangero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/07 19:09:45 by slangero          #+#    #+#             */
+/*   Updated: 2025/05/07 19:32:19 by slangero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+int is_valid_rgb_component(char *component)
+{
+	int i;
+	int value;
+
+	if (!component || component[0] == '\0')
+		return (-1);
+
+	i = 0;
+	while (component[i])
+	{
+		if (!ft_isdigit(component[i]))
+			return (-1);
+		i++;
+	}
+	value = ft_atoi(component);
+	if (value < 0 || value > 255)
+		return (-1);
+
+	return (value);
+}
+
+int parse_and_validate_rgb(char *rgb_string)
+{
+	char    **parts;
+	char    *trimmed_parts[3];
+	int     rgb[3];
+	int     i;
+	int     final_color;
+
+	if (!rgb_string) return (-1);
+
+	parts = ft_split(rgb_string, ',');
+	if (!parts) return (-1);
+	i = 0;
+	while (parts[i])
+		i++;
+	if (i != 3)
+	{
+		free_array(parts);
+		return (-1);
+	}
+	final_color = -1;
+	for (i = 0; i < 3; i++)
+	{
+		trimmed_parts[i] = ft_strtrim(parts[i], " \t\n");
+		if (!trimmed_parts[i]) {
+			 int k = 0;
+			 while(k < i) free(trimmed_parts[k++]);
+			 free_array(parts);
+			 return -1;
+		}
+		rgb[i] = is_valid_rgb_component(trimmed_parts[i]);
+		free(trimmed_parts[i]);
+
+		if (rgb[i] == -1)
+		{
+			free_array(parts);
+			return (-1);
+		}
+	}
+	final_color = (rgb[0] << 16) | (rgb[1] << 8) | rgb[2];
+	free_array(parts);
+	return (final_color);
+}
